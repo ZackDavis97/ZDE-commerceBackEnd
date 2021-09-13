afad2a9b81c75034c07a90b18340b3c8dbea94a4
Zack Davis <jantrezac95@gmail.com>
2021-09-13 13:37:29 -0500

    fixed some errors that broke product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,15 +8,11 @@ router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   Product.findAll({
-    attributes: ['id', 'product_name', 'price', 'stock'],
     include: [
-      {
-        model: Category,
-        attributes: ['category_name']
-      },
+      Category, 
       {
         model: Tag,
-        attributes: ['tag_name']
+        through: ProductTag
       }
     ]
   })
@@ -37,13 +33,10 @@ router.get('/:id', (req, res) => {
     },
     attributes: ['id', 'product_name', 'price', 'stock'],
     include: [
-      {
-        model: Category,
-        attributes: ['category_name']
-      },
+      Category, 
       {
         model: Tag,
-        attributes: ['tag_name']
+        through: Product_Tag
       }
     ]
   })
